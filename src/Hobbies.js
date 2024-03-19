17b7287440ea16aa26ff9cebebf14d6f531ea0dc
annarm994 <155604262+annarm994@users.noreply.github.com>
2024-03-19 20:00:12 +0500

    Add files via upload

---
+++ src/Hobbies.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Hobbies = () => {
+  return (
+    <div className="hobbies">
+      <h2>Hobbies</h2>
+      <ul>
+        <li>Coding</li>
+        <li>Reading</li>
+        <li>Hiking</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Hobbies;
---
+++ src/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className="profile">
+      <img src="profile.jpg" alt="Profile" />
+      <p>Hello, my name is Anna</p>
+    </div>
+  );
+};
+
+export default Profile;
